58068cdbca448c0a236d9d2ce91b78583fae4780
Nikita Bloshchanevich <nikblos@outlook.com>
2021-01-09 13:20:27 +0100

    Emacs cask plugin: modularize
    Split the cask plugin template into an Emacs plugin template without
    Cask, and make the cask plugin template `:inherit' from the former.

--- rsc/project-templates/Emacs/plugin/.ptemplate.el
+++ rsc/project-templates/Emacs/plugin/.ptemplate.el
@@ -7,9 +7,14 @@
 ;;; Code:
 
 (require 'ptemplate)
+(require 'ptemplate-templates)
 
 (ptemplate!
   :ignore "/README.md"
-  :inherit (ptemplate-source "../plugin/"))
+  :snippet-let
+  (ptemplate-var-pname (ptemplate-templates--project-name))
+  (ptemplate-var-module (concat ptemplate-var-pname ".el"))
+  :remap ("/src.el.yas" ptemplate-var-module)
+  :nokill ptemplate-var-module)
 
 ;;; .ptemplate.el ends here
